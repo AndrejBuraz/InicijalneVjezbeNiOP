0898f48f1667714d060b8802db472d6caab449c0
Andrej <75538726+AndrejBuraz@users.noreply.github.com>
2023-09-11 11:46:45 +0200

    Add files via upload

---
+++ InicijalnoPonavljanje/InicijalnoPonavljanje/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InicijalnoPonavljanje
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 100, b = 3;
+            Console.WriteLine("A dijeljeno s B je: " + (a/b) + ", i ostatak: " + (a%b));
+            Console.ReadKey();
+        }
+    }
+}
